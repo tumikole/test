d84a9a7cce3696ba1c234f3a3b64a41f2fd21513
tumikole <samuelramollo@gmail.com>
2021-04-22 04:31:41 +1000

    commiting htlm, css and js pages

---
+++ script.js
@@ -0,0 +1,10 @@
+const submitButton = (event) => {
+    event.preventDefault()
+ var name =document.querySelector('#fname').value;
+ var lname = document.querySelector('#lname').value;
+ var contact =document.querySelector('#contact').value;
+
+ 
+}
+
+submitButton()
